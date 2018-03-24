83ae909d95fb8989c956454df5c31d03a05c8875
Zhenwei Wu <wuzhenwei35@gmail.com>
2018-03-24 10:43:56 -0400

    Add a better rasterizer for recorder.

---
+++ FFTViewer/FFTImageRasterizer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTViewer
+{
+    class FFTImageRasterizer
+    {
+        public FFTImageRasterizer(int width, Color c0, Color c)
+        {
+            _Buffer = new int[width];
+            _C0 = c0;
+            _C = c;
+        }
+
+        private Color _C0, _C;
+
+        private int[] _Buffer;
+
+        private bool _HasLast;
+        private float _LastPixelX;
+        private float _LastPixelY;
+
+        private bool _HasCurrent;
+        private float _CurrentPixelX;
+        private float _CurrentPixelY;
+
+        public void Begin()
+        {
+            _HasLast = false;
+        }
+
+        public void End()
+        {
+            if (_HasCurrent)
+            {
+                //DrawPoint(_Buffer.Length, _LastPixelY);
+            }
+        }
+
+        public void DrawPoint(float x, float y)
+        {
+            if (!_HasLast)
+            {
+                _HasLast = true;
+                _LastPixelX = x;
+                _LastPixelY = y;
+                _HasCurrent = false;
+
+                for (int i = 0; i <= x; ++i)
+                {
+                    _Buffer[i] = _C0.ToArgb();
+                }
+                return;
+            }
+
+            if (!_HasCurrent)
+            {
+                _HasCurrent = true;
+                _CurrentPixelX = x;
+                _CurrentPixelY = y;
+            }
+            else
+            {
+                _CurrentPixelX = x;
+                _CurrentPixelY = Math.Max(y, _CurrentPixelY);
+            }
+
+            if (Math.Ceiling(_LastPixelX) < Math.Floor(_CurrentPixelX))
+            {
+                for (int i = (int)Math.Ceiling(_LastPixelX); i <= Math.Floor(_CurrentPixelX); ++i)
+                {
+                    if (i < 0)
+                    {
+                        continue;
+                    }
+                    if (i >= _Buffer.Length)
+                    {
+                        break;
+                    }
+                    float pos = (i - _LastPixelX) / (_CurrentPixelX - _LastPixelX);
+                    float drawY = _LastPixelY * (1 - pos) + _CurrentPixelY * pos;
+                    _Buffer[i] = GetColor(drawY);
+                }
+
+                _LastPixelX = _CurrentPixelX;
+                if (_LastPixelX == Math.Floor(_LastPixelX)) _LastPixelX += 1;
+                _LastPixelY = _CurrentPixelY;
+
+                _HasCurrent = false;
+            }
+        }
+
+        private int GetColor(float val)
+        {
+            var x = val;
+            if (x > 1) x = 1;
+            if (x < 0) x = 0;
+            var y = 1 - x;
+            var r = _C0.R * y + _C.R * x;
+            var g = _C0.G * y + _C.G * x;
+            var b = _C0.B * y + _C.B * x;
+            return Color.FromArgb((int)r, (int)g, (int)b).ToArgb();
+        }
+
+        public void CopyTo(IntPtr ptr)
+        {
+            Marshal.Copy(_Buffer, 0, ptr, _Buffer.Length);
+        }
+    }
+}
--- FFTViewer/FFTImageRecorder.cs
+++ FFTViewer/FFTImageRecorder.cs
@@ -14,21 +14,18 @@ namespace FFTViewer
         public FFTImageRecorder(int imageWidth, int imageHeight, float ratioY, Color c0, Color c)
         {
             _Image = new Bitmap(imageWidth, imageHeight, PixelFormat.Format32bppArgb);
-            _LineBuffer = new int[imageWidth];
+            _Rasterizer = new FFTImageRasterizer(imageWidth, c0, c);
             _LockedData = _Image.LockBits(new Rectangle(0, 0, imageWidth, imageHeight),
                 ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
             RatioY = ratioY;
-            _C0 = c0;
-            _C = c;
         }
         
         private Bitmap _Image;
         private BitmapData _LockedData;
         private int _PositionY;
-        private int[] _LineBuffer; //int32 argb values
+        private FFTImageRasterizer _Rasterizer;
         private bool _WrapImage = false;
         public float RatioY;
-        private Color _C0, _C;
 
         public bool Enabled = true;
 
@@ -42,31 +39,16 @@ namespace FFTViewer
             {
                 throw new InvalidOperationException();
             }
-
-            float pixelWidth = rect.Width / _Image.Width;
-            float pixelEnd = rect.Left + pixelWidth;
-            int lineBufferPosition = 0;
-            NewPixel();
+            
+            _Rasterizer.Begin();
             for (int i = 0; i < points.Length; ++i)
             {
                 var point = points[i];
-                if (point.X <= rect.Left) continue;
-                if (point.X >= rect.Right)
-                {
-                    break;
-                }
-                while (point.X > pixelEnd)
-                {
-                    pixelEnd += pixelWidth;
-                    _LineBuffer[lineBufferPosition++] = FinishPixel().ToArgb();
-                    NewPixel();
-                }
-                AddPixelData((rect.Bottom - point.Y) / rect.Height);
+                var x = (point.X - rect.Left) / rect.Width * _Image.Width;
+                var y = (rect.Bottom - point.Y) / rect.Height;
+                _Rasterizer.DrawPoint(x, y);
             }
-            if (lineBufferPosition < _LineBuffer.Length)
-            {
-                _LineBuffer[lineBufferPosition] = FinishPixel().ToArgb();
-            }
+            _Rasterizer.End();
 
             var ptr = IntPtr.Add(_LockedData.Scan0, _LockedData.Stride * _PositionY);
             _PositionY += 1;
@@ -75,8 +57,8 @@ namespace FFTViewer
                 _PositionY = 0;
                 _WrapImage = true;
             }
-
-            Marshal.Copy(_LineBuffer, 0, ptr, _LineBuffer.Length);
+            
+            _Rasterizer.CopyTo(ptr);
         }
 
         private RectangleF DrawBitmapSection(Graphics g, RectangleF rect, int start, int end)
@@ -139,50 +121,6 @@ namespace FFTViewer
             _Image = null;
             _LockedData = null;
             return ret;
-        }
-
-        private float _PixelAccumulate;
-        private float _LastPixelValue;
-
-        private void InitPixel()
-        {
-            _LastPixelValue = 0;
-        }
-
-        private void NewPixel()
-        {
-            _PixelAccumulate = -1;
-        }
-
-        private void AddPixelData(float val)
-        {
-            if (val > 1) val = 1;
-            if (val < 0) val = 0;
-            //Max
-            if (_PixelAccumulate < val) _PixelAccumulate = val;
-        }
-
-        private Color FinishPixel()
-        {
-            if (_PixelAccumulate < 0)
-            {
-                _PixelAccumulate = _LastPixelValue;
-            }
-            else
-            {
-                _LastPixelValue = _PixelAccumulate;
-            }
-            return Interpolate(_PixelAccumulate);
-        }
-
-        private Color Interpolate(float val)
-        {
-            var x = val;
-            var y = 1 - val;
-            var r = _C0.R * y + _C.R * x;
-            var g = _C0.G * y + _C.G * x;
-            var b = _C0.B * y + _C.B * x;
-            return Color.FromArgb((int)r, (int)g, (int)b);
         }
     }
 }
